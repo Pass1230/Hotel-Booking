1d4188c770bfe26251796e4a831021e3ac8b07f7
Yu <1051164697@qq.com>
2020-01-13 07:45:52 +0000

    initial deploy commit

--- app.js
+++ app.js
@@ -15,7 +15,15 @@ var HotelRoute = require("./routes/hotels.js"),
 	CommentRoute = require("./routes/comments.js"),
 	IndexRoute = require("./routes/index.js");
 
-mongoose.connect("mongodb://localhost:27017/hotel_booking", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://Yu:Aa1051164697@cluster0-mqu4f.mongodb.net/test?retryWrites=true&w=majority", {
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log("Connected to DB!");
+}).catch(err => {
+	console.log("ERROR:", err.message);
+});
+
 mongoose.set('useFindAndModify', true);
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
